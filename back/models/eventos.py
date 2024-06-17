ef00218e131ac63244e7bfbd5e80765ae8ee226c
Facu Briguera <facu.briguera12@gmail.com>
2024-06-17 18:36:53 -0300

    se agrega eventos

--- back/models/eventos.py
+++ back/models/eventos.py
@@ -1,2 +1,12 @@
 from pydantic import BaseModel, EmailStr
+from datetime import date
 
+class Events(BaseModel):
+    id: int
+    nombre: str
+    descripcion: str
+    fecha_inicio: date
+    fecha_fin: date
+    lugar: str
+    cupos: int
+    categoria_id: int
